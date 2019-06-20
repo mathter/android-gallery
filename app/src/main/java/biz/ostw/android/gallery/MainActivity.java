83f23dadf0e85ce067b786b8bb54bb1f1f19247e
mathter <mathter@mail.ru>
2019-06-20 16:30:15 +0700

    Import sources

---
+++ app/src/main/java/biz/ostw/android/gallery/MainActivity.java
@@ -0,0 +1,20 @@
+package biz.ostw.android.gallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements GridViewFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
